14f95d3365073f42ecb230b7028cd8347f089b8e
Danilopc <sr_danilo@hotmail.com>
2024-03-05 17:13:12 +0000

    path + glass effect added

---
+++ src/components/Background.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Background = ({ city }) => {
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    // fetch random image from Unsplash API
+    const fetchRandomImage = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.unsplash.com/photos/random",
+          {
+            params: {
+              query: city || "nature", 
+              client_id: "fLMtdfoAfkrFdsN3nmAra8NmQkOTRuguv6uikh0Ktfw",
+              w: 1920, 
+              h: 1080, 
+            },
+          }
+        );
+        setImageUrl(response.data.urls.regular);
+      } catch (error) {
+        console.error("Error fetching image:", error);
+      }
+    };
+
+    fetchRandomImage(); // Call the function when component 
+
+    // Clean function
+    return () => {
+   
+    };
+  }, [city]); // Trigger the effect when the city prop changes
+
+  return (
+    <div className="landing-page ">
+   
+      {imageUrl && <img src={imageUrl} alt="Random" />}
+    </div>
+  );
+};
+
+export default Background;
