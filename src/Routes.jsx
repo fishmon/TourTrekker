849ad09a8f4a13a499ee4b6ad79590d4e30d93e5
fishmon <31542001+fishmon@users.noreply.github.com>
2024-03-07 13:51:42 +0000

    header hiden from landing page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,30 +3,31 @@ import LandingPage from "./components/landingPage";
 import Forecast from "./components/Forecast";
 import Info from "./components/Info";
 import Places from "./components/Places";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Header from './components/Header';
 
-const AppRoutes = ({ city }) => {
-  // Component to manage routing
+const AppRoutes = ({ city, onSearch }) => {
+  const location = useLocation();
+  const showHeader = location.pathname !== '/'; // Check if current path is not the landing page
+
   return (
     <div>
-      <Router>
-        <Routes>
-          {/* Route for landing page */}
-          <Route path="/" element={<LandingPage city={city} />} />
+      {showHeader && <Header onSearch={onSearch} />} {/* Render the header only if it's not the landing page */}
+      <Routes>
+        {/* Route for landing page */}
+        <Route path="/" element={<LandingPage city={city} onSearch={onSearch} />} />
 
-          {/* Route for info page */}
-          <Route path="/info" element={<Info />} />
+        {/* Route for info page */}
+        <Route path="/info" element={<Info />} />
 
-          {/* Route for forecast page, passing city prop to Forecast */}
-          <Route path="/forecast" element={<Forecast city={city} />} />
+        {/* Route for forecast page, passing city prop to Forecast */}
+        <Route path="/forecast" element={<Forecast city={city} />} />
 
-          {/* Route for places page, passing city prop to Places */}
-          <Route path="/places" element={<Places city={city} />} />
-        </Routes>
-      </Router>
+        {/* Route for places page, passing city prop to Places */}
+        <Route path="/places" element={<Places city={city} />} />
+      </Routes>
     </div>
   );
 };
 
 export default AppRoutes;
-
