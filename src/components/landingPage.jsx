ea3134679a16bf2635b9b469943ff0257b85d222
Danilopc <sr_danilo@hotmail.com>
2024-03-07 15:51:00 +0000

    update

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -12,12 +12,12 @@ const LandingPage = () => {
   };
 
   return (
-    <div className="landing-page">
-      <div className="container position-absolute text-black welcome-box rounded">
+    <div className="landing-page d-flex justify-content-center align-items-center">
+      <div className="container text-black welcome-box rounded">
         <h2 className="bg fs-1">Welcome!</h2>
         <p>Embark on a journey of exploration and discovery with TourTrekker – your gateway to discovering the world's wonders. 
           <br/><b> Start</b> your journey today <b>by typing in a city name</b> and uncovering a world of possibilities. <br/>Happy exploring!</p>
-        <div className="d-flex search-input justify-content-center">
+        <div className="d-flex justify-content-center">
           {/* Button to navigate to the info page */}
           <button className="btn btn-search" onClick={handleNavigateToInfo}>Let's start!</button>
         </div>
