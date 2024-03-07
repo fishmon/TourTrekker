bcee41abdb1014f652cde8d6c60686dfac4e7f3a
fishmon <31542001+fishmon@users.noreply.github.com>
2024-03-07 10:07:20 +0000

    fixed forecast code

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,31 +1,17 @@
+// Forecast.js
 import React from 'react';
 import ForecastLogic from './ForecastLogic';
 import BottomButtons from './BottomButtons';
 
-function formatDate(timestamp) {
-  const date = new Date(timestamp * 1000); // Convert from Unix timestamp to milliseconds
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
-}
-
-
-
 function Forecast({ city }) {
-  const { currentWeather, forecastWeather, backgroundImageUrl } = ForecastLogic({ city });
-
-
-
-function Forecast({ city }) {
-  const { currentWeather, forecastWeather, formatDate } = ForecastLogic({ city });
+  const { currentWeather, forecastWeather, getWeatherData, formatDate } = ForecastLogic({ city });
 
   return (
     <div className="weather-app">
-  
-      {/* Current weather card */}
       <div className="row">
-        <div className="col-md-12 mb-3"> {/* Full width for the current weather card */}
+        <div className="col-md-12 mb-3">
           {currentWeather && (
-            <div className="card glass"> {/* Apply glass effect */}
+            <div className="card glass">
               <div className="card-body">
                 <h5 className="card-title">{currentWeather.name}</h5>
                 <p className="card-text">Temperature: {currentWeather.main.temp} °C</p>
@@ -38,12 +24,11 @@ function Forecast({ city }) {
         </div>
       </div>
 
-      {/* Forecast cards */}
       <div className="row">
         {forecastWeather.length > 0 && (
           forecastWeather.map((entry, index) => (
-            <div className="col-md-2 mb-3" key={index}> {/* Set column width to 2 (15% of 12-column grid) */}
-              <div className="card glass"> {/* Apply glass effect */}
+            <div className="col-md-2 mb-3" key={index}>
+              <div className="card glass">
                 <div className="card-body">
                   <h5 className="card-title">Date: {formatDate(entry.dt)}</h5>
                   <p className="card-text">Temperature: {entry.main.temp} °C</p>
